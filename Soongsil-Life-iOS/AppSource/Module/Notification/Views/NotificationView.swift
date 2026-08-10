830340a97904036d69a5b71e007d26446965ff1e
seok <ccdsles@gmail.com>
2026-08-10 22:54:53 +0900

    feat : 알림 탭 초기 구현

--- Soongsil-Life-iOS/AppSource/Module/Notification/Views/NotificationView.swift
+++ Soongsil-Life-iOS/AppSource/Module/Notification/Views/NotificationView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct NotificationView: View {
     var body: some View {
-        // TODO: 알림 요구사항과 서버 API가 준비되면 이 영역에 알림 화면을 구현합니다.
+        // TODO: 알림 요구사항과 서버 API가 준비되면 이 영역에 알림 화면을 구현합니다.제발 푸쉬 성공
         Color.soomsilBackground
             .ignoresSafeArea()
             .navigationTitle(L10n.Common.notifications)
